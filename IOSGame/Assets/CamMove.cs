8980cfeab3650aba0a9960a61cd98aff7841e346
Unliterate <trevor.waller@hotmail.ca>
2015-11-15 08:54:41 -0500

    - Hide build and library from git files - Added the game to repo

---
+++ IOSGame/Assets/CamMove.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamMove : MonoBehaviour
+{
+    Vector3 DesiredPosition = new Vector3(0, 0, -10);
+
+    Vector2 MinRanges = new Vector2(-10, -10);
+    Vector2 MaxRanges = new Vector2(10, 10);
+
+    Vector3 Velocity = Vector3.zero;
+
+    private void Update()
+    {
+        if (Input.touchCount > 1)
+        {
+            if (Input.touchCount == 2)
+            {
+                Touch touchZero = Input.GetTouch(0);
+                Touch touchOne = Input.GetTouch(1);
+
+                Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+                Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+                float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+                float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+                float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+                DesiredPosition.z -= deltaMagnitudeDiff * 0.01f;
+                DesiredPosition.z = Mathf.Clamp(DesiredPosition.z, -10, -2);
+            }
+        }
+        else if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                DesiredPosition.x -= Input.GetTouch(0).deltaPosition.x * 0.01f;
+                DesiredPosition.y -= Input.GetTouch(0).deltaPosition.y * 0.01f;
+
+                DesiredPosition.x = Mathf.Clamp(DesiredPosition.x, MinRanges.x, MaxRanges.x);
+                DesiredPosition.y = Mathf.Clamp(DesiredPosition.y, MinRanges.y, MaxRanges.y);
+            }
+        }
+
+        transform.position = Vector3.SmoothDamp(transform.position, DesiredPosition, ref Velocity, 0.1f);
+    }
+}
---
+++ IOSGame/Assets/Tile.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour
+{
+    public Material GreenMat = null;
+    public Material YellowMat = null;
+
+    public enum ETileType
+    {
+        TT_Green,
+        TT_Yellow
+    };
+
+    public ETileType TileType = ETileType.TT_Green;
+
+    private void Start()
+    {
+        if(Random.Range(0, 5) > 0)
+        {
+            TileType = ETileType.TT_Green;
+            GetComponent<Renderer>().material = GreenMat;
+        }
+        else
+        {
+            TileType = ETileType.TT_Yellow;
+            GetComponent<Renderer>().material = YellowMat;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        TileType = ETileType.TT_Green;
+        GetComponent<Renderer>().material = GreenMat;
+    }
+}
